4b403825c8c0a5c626252263c29e46bdc07b0b7e
Dylan Simowitz <dsimowitz@gmail.com>
2016-09-15 19:24:43 -0700

    use env variable for connection to db

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,14 +1,15 @@
 import knex from 'knex'
 const connection = knex({
   client: 'pg',
-  connection: {
-    host     : 'localhost',
-    port     : '32768',
-    user     : 'postgres',
-    password : 'password',
-    database : 'postgres',
-    charset  : 'utf8'
-  }
+  connection: process.env.DB_CONNECTION,
+  //connection: {
+    //host     : 'process.env.DB_HOST',
+    //port     : 'process.env.DB_PORT',
+    //user     : 'process.env.DB_USER',
+    //password : 'process.env.DB_PASSWORD',
+    //database : 'postgres',
+    //charset  : 'utf8'
+  //}
 });
 
 export default connection
