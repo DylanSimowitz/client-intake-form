e52f8d725016c387eb53745b38d9af6295415caa
Dylan Simowitz <dsimowitz@gmail.com>
2016-10-03 03:53:44 -0700

    add status to redirect

--- server/router/routes/verify.js
+++ server/router/routes/verify.js
@@ -8,11 +8,11 @@ router.get('/', (req, res) => {
   const {token} = req.query
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      res.end('Your token is invalid')
+      res.status(400).json({error: 'Your token is invalid'})
     }
     else {
       Client.where({email: decoded.email}).fetch({require: true}).then(client => {
-        client.set('verified', true).save().then(() => res.redirect('/login')) 
+        client.set('verified', true).save().then(() => res.status(302).redirect('/login')) 
       })
         .catch(Client.NotFoundError, () => res.status(400).redirect('/'))
     }
